bcd4df1387f43f720255bfd12b9a26e22af7e41c
Chris80310 <chris80780@yahoo.fr>
2023-11-21 16:54:39 +0100

    com2

--- SQL/bdd_fil_rouge.sql
+++ SQL/bdd_fil_rouge.sql
@@ -1,43 +1,44 @@
 CREATE DATABASE hardware_shop;
 USE hardware_shop;
-CREATE TABLE Commande(
-   Id_Commande INT AUTO_INCREMENT,
+
+CREATE TABLE commande (
+   id_com INT AUTO_INCREMENT,
    date_com DATE NOT NULL,
    total_com DECIMAL(7,2)   NOT NULL,
-   PRIMARY KEY(Id_Commande)
+   PRIMARY KEY(id_com)
 );
 
-CREATE TABLE Distributeurs(
-   Id_Distributeurs INT AUTO_INCREMENT,
-   logo_distri VARCHAR(50) ,
-   nom_distri VARCHAR(50)  NOT NULL,
-   adr_distri VARCHAR(100)  NOT NULL,
-   tel_distri VARCHAR(50)  NOT NULL,
-   PRIMARY KEY(Id_Distributeurs)
+CREATE TABLE distributeurs(
+   id_dis INT AUTO_INCREMENT,
+   logo_dis VARCHAR(50) ,
+   nom_dis VARCHAR(50)  NOT NULL,
+   adr_dis VARCHAR(100)  NOT NULL,
+   tel_dis VARCHAR(50)  NOT NULL,
+   PRIMARY KEY(id_dis)
 );
 
 
-CREATE TABLE Bon_livraison(
-   Id_bl INT AUTO_INCREMENT,
+CREATE TABLE B)bon_livraison(
+   id_bl INT AUTO_INCREMENT,
    date_livr DATE NOT NULL,
-   Id_Commande INT NOT NULL,
-   PRIMARY KEY(Id_bl),
-   FOREIGN KEY(Id_Commande) REFERENCES Commande(Id_Commande)
+   id_com INT NOT NULL,
+   PRIMARY KEY(id_bl),
+   FOREIGN KEY(id_com) REFERENCES commande (id_com)
 );
 
-CREATE TABLE Produit(
-   Id_Produit INT AUTO_INCREMENT,
+CREATE TABLE produit(
+   id_prod INT AUTO_INCREMENT,
    img_prod VARCHAR(50) ,
    libelle_prod VARCHAR(50)  NOT NULL,
    prix_achat_fourn DECIMAL(10,2)   NOT NULL,
    prix_vente_ht DECIMAL(10,2)   NOT NULL,
-   Id_Distributeurs INT,
-   PRIMARY KEY(Id_Produit),
-   FOREIGN KEY(Id_Distributeurs) REFERENCES Distributeurs(Id_Distributeurs)
+   id_dis INT,
+   PRIMARY KEY(id_prod),
+   FOREIGN KEY(id_dis) REFERENCES distributeurs(id_dis)
 );
 
-CREATE TABLE Utilisateurs(
-   Id_util INT AUTO_INCREMENT,
+CREATE TABLE utilisateurs(
+   id_util INT AUTO_INCREMENT,
    nom_entr VARCHAR(50) ,
    cli_nom VARCHAR(50)  NOT NULL,
    cli_prenom VARCHAR(50)  NOT NULL,
@@ -48,57 +49,71 @@ CREATE TABLE Utilisateurs(
    cli_adr_fact VARCHAR(100)  NOT NULL,
    cli_adr_fact_2 VARCHAR(100)  NOT NULL,
    util_mdp VARCHAR(50)  NOT NULL,
-   com_assigné BOOLEAN,
-   Id_util_1 INT,
-   Id_Commande INT,
-   PRIMARY KEY(Id_util),
-   FOREIGN KEY(Id_util_1) REFERENCES Utilisateurs(Id_util),
-   FOREIGN KEY(Id_Commande) REFERENCES Commande(Id_Commande)
+   com_assignee BOOLEAN,
+   id_util_1 INT,
+   id_Com INT,
+   PRIMARY KEY(id_util),
+   FOREIGN KEY(id_util_1) REFERENCES utilisateurs(id_util),
+   FOREIGN KEY(id_Com) REFERENCES commande(id_Com)
 );
 
-CREATE TABLE Facture(
-   Id_facture INT AUTO_INCREMENT,
+CREATE TABLE facture(
+   id_fact INT AUTO_INCREMENT,
    mode_paie VARCHAR(1)  NOT NULL,
    prix_ht DECIMAL(6,2)   NOT NULL,
    date_emi DATE NOT NULL,
    taux_tva DECIMAL(2,2)   NOT NULL,
    prix_total DECIMAL(6,2)   NOT NULL,
-   Id_Commande INT NOT NULL,
-   PRIMARY KEY(Id_facture),
-   FOREIGN KEY(Id_Commande) REFERENCES Commande(Id_Commande)
+   id_Com INT NOT NULL,
+   PRIMARY KEY(id_fact),
+   FOREIGN KEY(id_Com) REFERENCES commande(id_Com)
 );
 
-CREATE TABLE Catégorie(
-   Id_Catégorie INT AUTO_INCREMENT,
-   photo_cat VARCHAR(50) ,
+CREATE TABLE catégorie(
+   id_Cat INT AUTO_INCREMENT,
+   img_cat VARCHAR(50) ,
    libelle_cat VARCHAR(50) ,
-   Id_Produit INT NOT NULL,
-   PRIMARY KEY(Id_Catégorie),
-   FOREIGN KEY(Id_Produit) REFERENCES Produit(Id_Produit)
+   id_prod INT NOT NULL,
+   PRIMARY KEY(id_cat),
+   FOREIGN KEY(id_prod) REFERENCES Produit(id_prod)
 );
 
-CREATE TABLE Sous_cat(
-   Id_Sous_cat INT AUTO_INCREMENT,
-   Id_Catégorie INT NOT NULL,
-   PRIMARY KEY(Id_Sous_cat),
-   FOREIGN KEY(Id_Catégorie) REFERENCES Catégorie(Id_Catégorie)
+CREATE TABLE sous_cat(
+   id_ss_cat INT AUTO_INCREMENT,
+   id_cat INT NOT NULL,
+   PRIMARY KEY(id_ss_cat),
+   FOREIGN KEY(id_cat) REFERENCES Catégorie(id_cat)
 );
 
-CREATE TABLE detail_bl_produit(
-   Id_Produit INT,
-   Id_bl INT,
+CREATE TABLE sous_cat2(
+   id_ss_cat2 INT AUTO_INCREMENT,
+   id_ss_cat NOT NULL,
+   PRIMARY KEY(id_ss_cat2),
+   FOREIGN KEY(id_ss_cat) REFERENCES Catégorie(id_ss_cat)
+);
+
+CREATE TABLE details_bl(
+   id_Produit INT,
+   id_bl INT,
    qte DECIMAL(5,0)   NOT NULL,
-   PRIMARY KEY(Id_Produit, Id_bl),
-   FOREIGN KEY(Id_Produit) REFERENCES Produit(Id_Produit),
-   FOREIGN KEY(Id_bl) REFERENCES Bon_livraison(Id_bl)
+   PRIMARY KEY(id_prod, id_bl),
+   FOREIGN KEY(id_prod) REFERENCES Produit(id_prod),
+   FOREIGN KEY (id_bl) REFERENCES Bon_livraison(id_bl)
 );
 
-CREATE TABLE Detail_commande(
-   Id_Produit INT,
-   Id_Commande INT,
+CREATE TABLE Details_commande (
+   id_prod INT,
+   id_com INT,
    qte DECIMAL(5,0)   NOT NULL,
    prix DECIMAL(6,2)   NOT NULL,
-   PRIMARY KEY(Id_Produit, Id_Commande),
-   FOREIGN KEY(Id_Produit) REFERENCES Produit(Id_Produit),
-   FOREIGN KEY(Id_Commande) REFERENCES Commande(Id_Commande)
+   PRIMARY KEY(id_prod, id_com),
+   FOREIGN KEY(id_prod) REFERENCES Produit(id_prod),
+   FOREIGN KEY(id_com) REFERENCES commande(id_com)
 );
+
+-- catégorie : CPU / GPU / ...
+-- sous-cat : 1080p / 1440p / 4k
+-- sous-cat2 : CPU gaming / CPU professionnel 
+
+--INSERT INTO sous_cat (libelle_prod, prix_achat_fourn, prix_vente_ht, sous_cat)
+--VALUES ('RYZEN 7600X', 200, 250, 4);
