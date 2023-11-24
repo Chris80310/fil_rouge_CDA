8b6eeffd7f5fdaea632e8127e235234905008d8c
Chris80310 <chris80780@yahoo.fr>
2023-11-24 12:37:21 +0100

    com4

--- SQL/bdd_fil_rouge.sql
+++ SQL/bdd_fil_rouge.sql
@@ -71,7 +71,7 @@ CREATE TABLE facture(
    FOREIGN KEY(id_Com) REFERENCES commande(id_Com)
 );
 
-CREATE TABLE catégorie(
+CREATE TABLE categorie(
    id_Cat INT AUTO_INCREMENT,
    img_cat VARCHAR(50) ,
    libelle_cat VARCHAR(50) ,
@@ -84,14 +84,14 @@ CREATE TABLE id_ss_cat(
    id_ss_cat INT AUTO_INCREMENT,
    id_cat INT NOT NULL,
    PRIMARY KEY(id_ss_cat),
-   FOREIGN KEY(id_cat) REFERENCES catégorie(id_cat)
+   FOREIGN KEY(id_cat) REFERENCES categorie(id_cat)
 );
 
 CREATE TABLE sous_cat2(
    id_ss_cat2 INT AUTO_INCREMENT,
    id_ss_cat NOT NULL,
    PRIMARY KEY(id_ss_cat2),
-   FOREIGN KEY(id_ss_cat) REFERENCES catégorie(id_ss_cat)
+   FOREIGN KEY(id_ss_cat) REFERENCES id_ss_cat(id_ss_cat)
 );
 
 CREATE TABLE details_bl(
@@ -113,45 +113,46 @@ CREATE TABLE details_commande (
    FOREIGN KEY(id_com) REFERENCES commande(id_com)
 );
 
--- catégorie : CPU / GPU / ...
+-- categorie : CPU / GPU / ...
 -- sous-cat : 1080p / 1440p / 4k
 -- sous-cat2 : gaming / professionnel
 
---INSERT INTO distributeurs (nom_distri) VALUES ('AMD');
---INSERT INTO distributeurs (nom_distri) VALUES ('NVIDIA');
---INSERT INTO distributeurs (nom_distri) VALUES ('INTEL');
+INSERT INTO distributeurs (nom_distri) VALUES ('AMD');
+INSERT INTO distributeurs (nom_distri) VALUES ('NVIDIA');
+INSERT INTO distributeurs (nom_distri) VALUES ('INTEL');
 
---INSERT INTO catégorie (libel_cat) VALUES ('cartes graphiques');
---INSERT INTO catégorie (libel_cat) VALUES ('processeurs');
---INSERT INTO catégorie (libel_cat) VALUES ('cartes mères');
---INSERT INTO catégorie (libel_cat) VALUES ('refroidissement processeur');
---INSERT INTO catégorie (libel_cat) VALUES ('mémoire vive');
---INSERT INTO catégorie (libel_cat) VALUES ('mémoire de stockage');
---INSERT INTO catégorie (libel_cat) VALUES ('alimentations');
---INSERT INTO catégorie (libel_cat) VALUES ('boitiers');
---INSERT INTO catégorie (libel_cat) VALUES ('cablage');
---INSERT INTO catégorie (libel_cat) VALUES ('ventilateurs boitier');
+INSERT INTO categorie (libel_cat) VALUES ('cartes graphiques'); --1
+INSERT INTO categorie (libel_cat) VALUES ('processeurs'); --2
+INSERT INTO categorie (libel_cat) VALUES ('refroidissement processeur'); --3
+INSERT INTO categorie (libel_cat) VALUES ('cartes mères'); --4
+INSERT INTO categorie (libel_cat) VALUES ('mémoire vive'); --5
+INSERT INTO categorie (libel_cat) VALUES ('mémoire de stockage'); --6
+INSERT INTO categorie (libel_cat) VALUES ('alimentations'); --7
+INSERT INTO categorie (libel_cat) VALUES ('boitiers'); --8
+INSERT INTO categorie (libel_cat) VALUES ('cablage'); --9
+INSERT INTO categorie (libel_cat) VALUES ('ventilateurs boitier'); --10
 
---INSERT INTO produit (libelle_prod, prix_achat_fourn, prix_vente_ht, id_ss_cat) VALUES ('RX6700XT', 235, 290, 1);
---INSERT INTO produit (libelle_prod, prix_achat_fourn, prix_vente_ht, id_ss_cat) VALUES ('RX6800XT', 380, 450, 1);
---INSERT INTO produit (libelle_prod, prix_achat_fourn, prix_vente_ht, id_ss_cat) VALUES ('RX7900XTX', 750, 900, 1);
+INSERT INTO produit (libelle_prod, prix_achat_fourn, prix_vente_ht, id_ss_cat) VALUES ('RX6700XT', 235, 290, 1),('RX6800XT', 380, 450, 1),('RX7900XTX', 750, 900, 1);
+INSERT INTO produit (libelle_prod, prix_achat_fourn, prix_vente_ht, id_ss_cat) VALUES ('RTX3060Ti', 250, 330, 1),('RTX3080', 450, 580, 1),('RTX4090', 1400, 1600, 1);
+INSERT INTO produit (libelle_prod, prix_achat_fourn, prix_vente_ht, id_ss_cat) VALUES ('ARC770', 250, 330, 1);
 
---INSERT INTO produit (libelle_prod, prix_achat_fourn, prix_vente_ht, id_ss_cat) VALUES ('RTX3060Ti', 250, 330, 2);
---INSERT INTO produit (libelle_prod, prix_achat_fourn, prix_vente_ht, id_ss_cat) VALUES ('RTX3080', 450, 580, 2);
---INSERT INTO produit (libelle_prod, prix_achat_fourn, prix_vente_ht, id_ss_cat) VALUES ('RTX4090', 1400, 1600, 2);
+INSERT INTO produit (libelle_prod, prix_achat_fourn, prix_vente_ht, id_ss_cat) VALUES ('RYZEN 7600X', 200, 250, 2),('RYZEN 7950X', 400, 550, 2),('RYZEN 5800X3D ', 200, 250, 2),('I5 13600k', 250, 330, 2),('I5 14900k', 600, 750, 2);
 
---INSERT INTO produit (libelle_prod, prix_achat_fourn, prix_vente_ht, id_ss_cat) VALUES ('ARC770', 250, 330, 3);
+INSERT INTO produit (libelle_prod, prix_achat_fourn, prix_vente_ht, id_ss_cat) VALUES ('Artic liquid freezer II', 80, 100, 3),('Noctua NH-D15 chromax', 80, 100, 3),('Thermalright Assassin X 120', 10, 15, 3);
 
---INSERT INTO produit (libelle_prod, prix_achat_fourn, prix_vente_ht, id_ss_cat) VALUES ('RYZEN 7600X', 200, 250, 4);
---INSERT INTO produit (libelle_prod, prix_achat_fourn, prix_vente_ht, id_ss_cat) VALUES ('RYZEN 7950X', 400, 550, 4);
---INSERT INTO produit (libelle_prod, prix_achat_fourn, prix_vente_ht, id_ss_cat) VALUES ('RYZEN 5800X3D ', 200, 250, 4);
---INSERT INTO produit (libelle_prod, prix_achat_fourn, prix_vente_ht, id_ss_cat) VALUES ('I5 13600k', 250, 330, 4);
---INSERT INTO produit (libelle_prod, prix_achat_fourn, prix_vente_ht, id_ss_cat) VALUES ('I5 14900k', 600, 750, 4);
+INSERT INTO produit (libelle_prod, prix_achat_fourn, prix_vente_ht, id_ss_cat) VALUES ('Asus TUF B660-Plus Wifi', 80, 150, 4),('MSI MAG B650 Tomahawk WiFi', 80, 150, 4);
 
---INSERT INTO produit (libelle_prod, prix_achat_fourn, prix_vente_ht, id_ss_cat) VALUES ('B660', 300, 380, 5);
---INSERT INTO produit (libelle_prod, prix_achat_fourn, prix_vente_ht, id_ss_cat) VALUES ('B650', 300, 380, 5);
+INSERT INTO produit (libelle_prod, prix_achat_fourn, prix_vente_ht, id_ss_cat) VALUES ('Corsair Vengeance LPX Kit 32 Go DDR4-3200 CL16', 45, 60, 5),('G.Skill Ripjaws S5 32 Go Kit DDR5-6000 CL30', 80, 120, 5);
 
---INSERT INTO produit (libelle_prod, prix_achat_fourn, prix_vente_ht, id_ss_cat) VALUES ('Artic liquid freezer II', 80, 100, 6);
+INSERT INTO produit (libelle_prod, prix_achat_fourn, prix_vente_ht, id_ss_cat) VALUES ('Samsung 980 Pro M.2 2 To', 80, 100, 6);
+
+INSERT INTO produit (libelle_prod, prix_achat_fourn, prix_vente_ht, id_ss_cat) VALUES ('MSI MAG GL A750GL 750W', 70, 90, 7),('Seasonic Focus GX-1000W ATX 3.0', 100, 130, 7);
+
+INSERT INTO produit (libelle_prod, prix_achat_fourn, prix_vente_ht, id_ss_cat) VALUES ('Phanteks Eclipse G360A Black', 50, 80, 8),('Lian Li O11 Dynamic EVO White', 90, 120, 8);
+
+INSERT INTO produit (libelle_prod, prix_achat_fourn, prix_vente_ht, id_ss_cat) VALUES ('Corsair Premium Individually Sleeved PSU Cables Pro Kit Type 4 Gen 4 - White/Black', 50, 80, 9);
+
+INSERT INTO produit (libelle_prod, prix_achat_fourn, prix_vente_ht, id_ss_cat) VALUES ('Arctic P12 PWM PST black Value Pack 120mm', 15, 25, 11),('Corsair iCUE Link QX120 RGB 120mm', 90, 120, 10);
 
 
 
